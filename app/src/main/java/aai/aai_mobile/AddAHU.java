135c01397b8d6281fc3163a4e4145bc4b5544928
genesis7773 <jwhitejob10@gmail.com>
2016-01-05 23:09:55 -0500

    upload current

---
+++ app/src/main/java/aai/aai_mobile/AddAHU.java
@@ -0,0 +1,48 @@
+package aai.aai_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+
+public class AddAHU extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ahu);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_ahu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void clearText(){
+
+        EditText addAHU = (EditText) findViewById(R.id.txtAddAHU);
+        addAHU.setText("");
+    }
+}
